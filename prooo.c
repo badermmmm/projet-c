6f49945a544cf66788433242b59efae0d88cdec4
Abdell931 <abdel.hafiz2007@gmail.com>
2025-10-15 23:21:40 +0200

    Implement user commands for student registration
    Added functionality to exit and register students based on user input.

--- prooo.c
+++ prooo.c
@@ -3,7 +3,7 @@
 #pragma warning (disable:4996)
 
 
-enum {Max_Etudiants=100};
+enum { Max_Etudiants = 100 };
 
 
 typedef struct {
@@ -14,7 +14,7 @@ typedef struct {
 void EXIT() {
 	char mot[100];
 	while (1) {
-		scanf("%s",&mot);
+		scanf("%s",mot);
 
 		if (strcmp(mot,"EXIT") == 0) {
 			break;
@@ -24,26 +24,34 @@ void EXIT() {
 void INSCRIRE(Etudiant etudiants[Max_Etudiants], int* nb) {
 	Etudiant e;
 
-	scanf("%s %s", e.prenom, e.nom);
+	scanf("%s %s",e.prenom,e.nom);
 
 	for (int i = 0; i < Max_Etudiants; ++i) {
-		if (strcmp(e.prenom,etudiants[i].prenom)==0 && strcmp(e.nom, etudiants[i].nom) == 0) {
+		if (strcmp(e.prenom, etudiants[i].prenom) == 0 && strcmp(e.nom, etudiants[i].nom) == 0) {
 			printf("##ERROR##\n");
 			return;
 		}
 	}
 
 	etudiants[*nb] = e;
-	printf("Inscription enregistree (%d)", *nb + 1);
-	++*nb;	
+	++*nb;
+	printf("Inscription enregistree (%d)\n", *nb);
 }
 
 int main() {
 	Etudiant etudiants[Max_Etudiants];
 	int nb_etudiants = 0;
+	char mot[30];
+	while (1) {
+		scanf("%s", mot);
 
-	//EXIT();
-	while(1)
-		INSCRIRE(etudiants, &nb_etudiants);
+		if (strcmp(mot, "EXIT") == 0) {
+			break;
+		}
+		else if (strcmp(mot, "INSCRIRE") == 0) {
+			INSCRIRE(etudiants, &nb_etudiants);
+		}
+	}
 
 }
+
