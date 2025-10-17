50d149696417dfd861decfa58135e868f00b4362
Abdell931 <abdel.hafiz2007@gmail.com>
2025-10-17 20:19:29 +0200

    Update prooo.c

--- prooo.c
+++ prooo.c
@@ -1,91 +1,83 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#pragma warning (disable:4996)
-enum {
-	MAX_ETUDIANTS = 100,
-	MAX_NOM = 31
-};
-typedef struct {
-	int id;// identifiant de l'étudiant
-	char prenom[MAX_NOM];// prenom de l'étudiant
-	char nom[MAX_NOM];// nom de l'étudiant
-	int semestres;// semestres de l'étudiant
-} Etudiant;
-typedef struct {
-	int nbEtudiants;
-	Etudiant etudiants[MAX_ETUDIANTS];
-} Promotion;
-
+#pragma warning(disable:4996)
 
 enum {
-	MAX_ETUDIANTS = 100,
-	NB_UE = 6, NB_SEMESTRES = 6,
-	MAX_NOM = 31
+    MAX_ETUDIANTS = 100,
+    MAX_NOM = 31,
+    NB_UE = 6,
+    NB_SEMESTRES = 6
 };
+
 const float NOTE_MIN = 0.f, NOTE_MAX = 20.f;
 
+typedef struct {
+    int id;
+    char prenom[MAX_NOM];
+    char nom[MAX_NOM];
+    int semestres;
+} Etudiant;
 
+void INSCRIRE(Etudiant etudiants[MAX_ETUDIANTS], int* nb) {
+    Etudiant e;
+    scanf("%s %s", e.prenom, e.nom);
 
-void EXIT() {
-	char mot[100];
-	while (1) {
-		scanf("%s",mot);
+    // Vérifie si l'étudiant existe déjà
+    for (int i = 0; i < *nb; ++i) {
+        if (strcmp(e.prenom, etudiants[i].prenom) == 0 &&
+            strcmp(e.nom, etudiants[i].nom) == 0) {
+            printf("Nom incorrect\n");
+            return;
+        }
+    }
 
-		if (strcmp(mot,"EXIT") == 0) {
-			break;
-		}
-	}
-}
-// variable qui sert à inscrire un étudiant avec son nom prenom et lui donne un id 
-void INSCRIRE(Etudiant etudiants[MAX_ETUDIANTS], int* id) {
-	Etudiant e;
-
-	scanf("%s %s",e.prenom,e.nom);
-
-	for (int i = 0; i < MAX_ETUDIANTS; ++i) {
-		if (strcmp(e.prenom, etudiants[i].prenom) == 0 && strcmp(e.nom, etudiants[i].nom) == 0) {
-			printf("##ERROR##\n");
-			return;
-		}
-	}
-
-	etudiants[*id] = e;
-	++*id;
-	printf("Inscription enregistree (%d)\n", *id);
-}
-// variable qui sert ou plutot qui servira quand elle marchera a mettre des notes aux etudiants dans un tableau de 6/6 pour les 6 semstres differents et les 3années 
-void NOTE(Etudiant *p,int id,int ue,float note) {
-	scanf("%d %d %f", id, ue, note);
-	for (int i = 0; i < NB_SEMESTRES; ++i) {
-		
-	}
+    e.id = *nb;
+    e.semestres = 0; // initialisation
+    etudiants[*nb] = e;
+    ++(*nb);
+    printf("Inscription enregistree (%d)\n", *nb);
 }
 
+void NOTE(Etudiant etudiants[MAX_ETUDIANTS], int *nb_etudiants) {
+    int id, ue;
+    float note;
+    scanf("%d %d %f",&id,&ue,&note);
 
-// la fonction principale qui utilise toute les autres fonctions
+    if (id < 1 || id >= *nb_etudiants) {
+        printf("Identifiant incorrect\n");
+        return;
+    }
+    if (ue < 1 || ue > NB_UE) {
+        printf("UE incorrecte\n");
+        return;
+    }
+    if (note < NOTE_MIN || note > NOTE_MAX) {
+        printf("Note incorrecte\n");
+        return;
+    }
+    printf("Note enregistree\n");
+}
+
 int main() {
-	Etudiant etudiants[MAX_ETUDIANTS];
-	int nb_etudiants = 0;
-	char mot[30];
-	Promotion p;
-	p.nbEtudiants = 0;
-	int note;
-	int id;
-	// boucle de l’ interpr´eteur de commande
-	while (1) {
-		scanf("%s",mot);
+    Etudiant etudiants[MAX_ETUDIANTS];
+    int nbEtudiants = 0;
+    char mot[30];
 
-		if (strcmp(mot,"EXIT") == 0) {
-			break;
-		}
-		else if (strcmp(mot, "INSCRIRE") == 0) {
-			INSCRIRE(etudiants, &nb_etudiants);
-		}
-		else if (strcmp(mot,"NOTE") == 0) {
-			NOTE(id, note ) ;
-		}
-	}
+    while (1) {
+        scanf("%s", mot);
+
+        if (strcmp(mot, "EXIT") == 0) {
+            break;
+        }
+        else if (strcmp(mot, "INSCRIRE") == 0) {
+            INSCRIRE(etudiants, &nbEtudiants);
+        }
+        else if (strcmp(mot, "NOTE") == 0) {
+            NOTE(etudiants,&nbEtudiants);
+        }
+    }
 
 }
+
 
