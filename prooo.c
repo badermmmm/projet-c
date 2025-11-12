c03f6271a4cfa37ede59dcd495ca63e4613cdf31
badermmmm <bader.mouneimneh@etu.u-paris.fr>
2025-11-12 13:54:59 +0100

    Update prooo.c

--- prooo.c
+++ prooo.c
@@ -18,37 +18,36 @@ typedef struct {
     char nom[MAX_NOM];
     int semestres;
     float notes[NB_SEMESTRES][NB_UE];
-    int hors_formation; // 0=en cours, 1=demission, 2=defaillance
+    int formation; // 0=en cours, 1=demission, 2=defaillance
 } Etudiant;
 
-// --- INSCRIRE ---
-void INSCRIRE(Etudiant etudiants[MAX_ETUDIANTS], int* nb) {
+// fonction qui permet dinscrire un eleve 
+void Inscrire(Etudiant etudiants[MAX_ETUDIANTS], int* nb) {
     Etudiant e;
     scanf("%s %s", e.prenom, e.nom);
 
     for (int i = 0; i < *nb; ++i) {
+
         if (strcmp(e.prenom, etudiants[i].prenom) == 0 &&
             strcmp(e.nom, etudiants[i].nom) == 0) {
             printf("Nom incorrect\n");
             return;
         }
     }
-
     e.id = *nb + 1;
     e.semestres = 1;
-    e.hors_formation = 0;
+    e.formation = 0;
 
     for (int s = 0; s < NB_SEMESTRES; s++)
         for (int u = 0; u < NB_UE; u++)
             e.notes[s][u] = -1.0f;
-
     etudiants[*nb] = e;
     ++(*nb);
     printf("Inscription enregistree (%d)\n", *nb);
 }
 
-// --- NOTE ---
-void NOTE(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
+// fonction qui permet de mettre des notes aux eleves inscrit 
+void Note(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
     int id, ue;
     float note;
     scanf("%d %d %f", &id, &ue, &note);
@@ -57,18 +56,19 @@ void NOTE(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
         printf("Identifiant incorrect\n");
         return;
     }
+
     if (ue < 1 || ue > NB_UE) {
         printf("UE incorrecte\n");
         return;
     }
+
     if (note < NOTE_MIN || note > NOTE_MAX) {
         printf("Note incorrecte\n");
         return;
     }
 
     Etudiant* e = &etudiants[id - 1];
-
-    if (e->hors_formation) {
+    if (e->formation) {
         printf("Etudiant hors formation\n");
         return;
     }
@@ -77,8 +77,8 @@ void NOTE(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
     printf("Note enregistree\n");
 }
 
-// --- CURSUS ---
-void CURSUS(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
+// fonction qui affiche l'eleve et ses notes 
+void Cursus(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
     int id;
     scanf("%d", &id);
 
@@ -90,48 +90,54 @@ void CURSUS(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
     Etudiant* e = &etudiants[id - 1];
 
     printf("%d %s %s\n", e->id, e->prenom, e->nom);
-
+//Affiche les notes de l'eleve
     for (int s = 0; s < e->semestres; s++) {
         printf("S%d - ", s + 1);
+
         for (int ue = 0; ue < NB_UE; ue++) {
             float note = e->notes[s][ue];
+
             if (note < 0)
                 printf("* (*)");
+
             else {
-                float nAffiche = floorf(note * 10) / 10;  // 🔹 tronquer à 1 chiffre sans arrondir
+                float nAffiche = floorf(note * 10) / 10;
                 const char* code = (note >= 10.0f) ? "ADM" : "AJ";
                 printf("%.1f (%s)", nAffiche, code);
             }
+
             if (ue < NB_UE - 1) printf(" - ");
         }
 
-        // Affichage du statut à la fin du semestre
-        if (e->hors_formation == 1)
+// Affiche le statut de l'eleve
+        if (e->formation == 1)
             printf(" - demission\n");
-        else if (e->hors_formation == 2)
+
+        else if (e->formation == 2)
             printf(" - defaillance\n");
+
         else
             printf(" - en cours\n");
     }
 }
 
-// --- ETUDIANTS ---
-void ETUDIANTS(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
+// fonction qui affiche tout les eleves le semestre et l'etat de l'eleve
+void Etudiants(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
     for (int i = 0; i < nb_etudiants; i++) {
         Etudiant e = etudiants[i];
         char statut[20] = "en cours";
 
-        if (e.hors_formation == 1)
+        if (e.formation == 1)
             strcpy(statut, "demission");
-        else if (e.hors_formation == 2)
+        else if (e.formation == 2)
             strcpy(statut, "defaillance");
 
         printf("%d - %s %s - S%d - %s\n", e.id, e.prenom, e.nom, e.semestres, statut);
     }
 }
 
-// --- DEMISSION ---
-void DEMISSION(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
+// fonction qui permet qui permet de mettre en demission un eleve
+void Demission(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
     int id;
     scanf("%d", &id);
 
@@ -142,17 +148,17 @@ void DEMISSION(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
 
     Etudiant* e = &etudiants[id - 1];
 
-    if (e->hors_formation) {
+    if (e->formation) {
         printf("Etudiant hors formation\n");
         return;
     }
 
-    e->hors_formation = 1;
+    e->formation = 1;
     printf("Demission enregistree\n");
 }
 
-// --- DEFAILLANCE ---
-void DEFAILLANCE(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
+//  fonction qui permet de stopper la scolarite d'un eleve 
+void Defaillance(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
     int id;
     scanf("%d", &id);
 
@@ -163,21 +169,21 @@ void DEFAILLANCE(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
 
     Etudiant* e = &etudiants[id - 1];
 
-    if (e->hors_formation) {
+    if (e->formation) {
         printf("Etudiant hors formation\n");
         return;
     }
 
-    e->hors_formation = 2;
+    e->formation = 2;
     printf("Defaillance enregistree\n");
 }
 
 // --- JURY ---
-void JURY(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
-    int semestre_courant;
-    scanf("%d", &semestre_courant);
+void Jury(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
+    int sem_actu;
+    scanf("%d", &sem_actu);
 
-    if (semestre_courant < 1 || semestre_courant > NB_SEMESTRES) {
+    if (sem_actu < 1 || sem_actu > NB_SEMESTRES) {
         printf("Semestre incorrect\n");
         return;
     }
@@ -188,14 +194,14 @@ void JURY(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
     for (int i = 0; i < nb_etudiants; i++) {
         Etudiant* e = &etudiants[i];
 
-        if (e->hors_formation) continue;
-        if (e->semestres != semestre_courant) continue;
+        if (e->formation) continue;
+        if (e->semestres != sem_actu) continue;
 
         etudiants_concernes++;
 
         int complet = 1;
         for (int ue = 0; ue < NB_UE; ue++) {
-            if (e->notes[semestre_courant - 1][ue] < 0) {
+            if (e->notes[sem_actu - 1][ue] < 0) {
                 complet = 0;
                 break;
             }
@@ -206,7 +212,7 @@ void JURY(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
     }
 
     if (etudiants_concernes == 0) {
-        printf("Aucun etudiant au semestre %d\n", semestre_courant);
+        printf("Aucun etudiant au semestre %d\n", sem_actu);
         return;
     }
 
@@ -216,7 +222,7 @@ void JURY(Etudiant etudiants[MAX_ETUDIANTS], int nb_etudiants) {
         printf("Semestre termine pour %d etudiant(s)\n", etudiants_concernes);
 }
 
-// --- MAIN ---
+// le main qui permet d'utiliser toutes les fonctions
 int main() {
     Etudiant etudiants[MAX_ETUDIANTS];
     int nbEtudiants = 0;
@@ -226,12 +232,26 @@ int main() {
         scanf("%s", mot);
 
         if (strcmp(mot, "EXIT") == 0) break;
-        else if (strcmp(mot, "INSCRIRE") == 0) INSCRIRE(etudiants, &nbEtudiants);
-        else if (strcmp(mot, "NOTE") == 0) NOTE(etudiants, nbEtudiants);
-        else if (strcmp(mot, "CURSUS") == 0) CURSUS(etudiants, nbEtudiants);
-        else if (strcmp(mot, "ETUDIANTS") == 0) ETUDIANTS(etudiants, nbEtudiants);
-        else if (strcmp(mot, "DEMISSION") == 0) DEMISSION(etudiants, nbEtudiants);
-        else if (strcmp(mot, "DEFAILLANCE") == 0) DEFAILLANCE(etudiants, nbEtudiants);
-        else if (strcmp(mot, "JURY") == 0) JURY(etudiants, nbEtudiants);
+
+        else if (strcmp(mot, "INSCRIRE") == 0) 
+            Inscrire(etudiants, &nbEtudiants);
+
+        else if (strcmp(mot, "NOTE") == 0) 
+            Note(etudiants, nbEtudiants);
+
+        else if (strcmp(mot, "CURSUS") == 0) 
+            Cursus(etudiants, nbEtudiants);
+
+        else if (strcmp(mot, "ETUDIANTS") == 0) 
+            Etudiants(etudiants, nbEtudiants);
+
+        else if (strcmp(mot, "DEMISSION") == 0) 
+            Demission(etudiants, nbEtudiants);
+
+        else if (strcmp(mot, "DEFAILLANCE") == 0) 
+            Defaillance(etudiants, nbEtudiants);
+
+        else if (strcmp(mot, "JURY") == 0)
+            Jury(etudiants, nbEtudiants);
     }
 }
